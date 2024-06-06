530340ad1b87acab91e9e6eac751e8e33614d4c8
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2024-06-07 01:40:36 +0530

    Create ArrangingCoins.java

---
+++ Problem Practic/BinarySearch/ArrangingCoins.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/arranging-coins/description/
+
+
+class Solution {
+    public int arrangeCoins(int n) {
+        int count = 0;
+        while(n>count){
+            count++;
+            n = n-count;
+        }
+        return count;
+    }
+}
