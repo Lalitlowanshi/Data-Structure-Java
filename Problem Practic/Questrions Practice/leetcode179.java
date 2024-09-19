2fb89c62fda03bb76ec5abdf3868ba680b4b9dff
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2024-09-19 23:08:30 +0530

    Add files via upload

---
+++ Problem Practic/Questrions Practice/leetcode179.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class leetcode179 {
+    public static void main(String[] args) {
+        int [] arr = {10,2};
+        System.out.println(largestNumber(arr));
+    }
+        public static String largestNumber(int[] nums) {
+            int n = nums.length;
+            String [] s = new String[n];
+            for(int i = 0; i<n; i++){
+                s[i] = String.valueOf(nums[i]);
+            }
+            Arrays.sort(s, (a,b) -> (b+a).compareTo(a+b));
+    
+            StringBuilder sb = new StringBuilder();
+            for(String str :s){
+                sb.append(str);
+            }
+            String result = sb.toString();
+            return result.startsWith("0") ? "0" : result;
+        }
+    }
