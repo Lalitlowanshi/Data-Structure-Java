ba2d565e2225002ae37aa298b7dd478aff817a90
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2024-10-19 18:55:59 +0530

    Create leetcode1545.java

---
+++ Problem Practic/Questrions Practice/leetcode1545.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        StringBuilder s = new StringBuilder("0");
+        
+        for (int i = 2; i <= n; i++) {
+          
+            StringBuilder temp = new StringBuilder(s);
+            
+            for (int j = 0; j < temp.length(); j++) {
+                temp.setCharAt(j, temp.charAt(j) == '0' ? '1' : '0');
+            }
+            temp.reverse();
+            
+            s.append("1").append(temp);
+        }
+        
+        return s.charAt(k - 1);
+    }
+}
