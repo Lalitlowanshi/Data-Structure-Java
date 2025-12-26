9a4c50e8be097a07e86621ef18a2b30b8f0c8386
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2025-12-26 18:50:18 +0530

    Create leetcode131.java

---
+++ Problem Practic/Striver Blind Sheet/leetcode131.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, 
+                           List<String> path, 
+                           List<List<String>> result) {
+
+        // base case
+        if (start == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        // try all substrings
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                path.add(s.substring(start, end + 1));
+                backtrack(s, end + 1, path, result);
+                path.remove(path.size() - 1); // backtrack
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
