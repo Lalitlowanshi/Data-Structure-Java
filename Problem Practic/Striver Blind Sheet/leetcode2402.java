0902953f7fcff3875f4763d1f9b024bb62a7863d
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2025-12-27 17:39:09 +0530

    Update leetcode2402.java

--- Problem Practic/Striver Blind Sheet/leetcode2402.java
+++ Problem Practic/Striver Blind Sheet/leetcode2402.java
@@ -22,4 +22,39 @@ class Solution {
         int[] count = new int[n];
 
         for (int[] meeting : meetings) {
-            long st
+            long start = meeting[0];
+            long end = meeting[1];
+            long duration = end - start;
+
+            // Free rooms that have finished
+            while (!busyRooms.isEmpty() && busyRooms.peek()[0] <= start) {
+                availableRooms.offer((int) busyRooms.poll()[1]);
+            }
+
+            if (!availableRooms.isEmpty()) {
+                // Assign normally
+                int room = availableRooms.poll();
+                busyRooms.offer(new long[]{end, room});
+                count[room]++;
+            } else {
+                // Delay meeting
+                long[] earliest = busyRooms.poll();
+                long newEnd = earliest[0] + duration;
+                int room = (int) earliest[1];
+
+                busyRooms.offer(new long[]{newEnd, room});
+                count[room]++;
+            }
+        }
+
+        // Find room with maximum meetings
+        int maxRoom = 0;
+        for (int i = 1; i < n; i++) {
+            if (count[i] > count[maxRoom]) {
+                maxRoom = i;
+            }
+        }
+
+        return maxRoom;
+    }
+}
