b832104aba838599e5a2b94ee8668568e2bbd35a
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-04-18 21:17:48 +0530

    Update inOrderTraversing.java

--- Problem Practic/Tree/inOrderTraversing.java
+++ Problem Practic/Tree/inOrderTraversing.java
@@ -1,1 +1,36 @@
 
+import java.util.*;
+public class Traversal {
+
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.right.left = new Node(4);
+
+        preorder(root);
+        System.out.println();
+        inorder(root);
+        System.out.println();
+        postorder(root);
+        System.out.println();
+        System.out.println(levelOrderTraversal(root));
+    }
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
