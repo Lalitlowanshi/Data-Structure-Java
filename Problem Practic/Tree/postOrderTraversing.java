9c67b09072df9d3435b3fda917d0bdca564a5db7
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-04-18 21:19:29 +0530

    Update postOrderTraversing.java

--- Problem Practic/Tree/postOrderTraversing.java
+++ Problem Practic/Tree/postOrderTraversing.java
@@ -1,1 +1,35 @@
-...
+import java.util.*;
+public class Traversal {
+
+    public static void postorder(Node root){
+        if(root == null)
+            return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.right.left = new Node(4);
+
+        preorder(root);
+        System.out.println();
+        inorder(root);
+        System.out.println();
+        postorder(root);
+        System.out.println();
+        System.out.println(levelOrderTraversal(root));
+    }
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
