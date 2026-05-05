d5cf34b7d37a99c536ede85c0c8712472e1a1e2c
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-05-05 16:20:56 +0530

    Create leetcode61.java

---
+++ Problem Practic/Questrions Practice/leetcode61.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        // Step 1: find length and tail
+        ListNode curr = head;
+        int length = 1;
+
+        while (curr.next != null) {
+            curr = curr.next;
+            length++;
+        }
+
+        // curr is tail
+        curr.next = head;  // Step 2: make circular
+
+        // Step 3: find new tail
+        k = k % length;
+        int stepsToNewTail = length - k - 1;
+
+        ListNode newTail = head;
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        // Step 4: break the circle
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
