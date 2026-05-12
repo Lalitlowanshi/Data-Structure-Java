546c55128707b691fe1ef9bce3272d6f3e2ddd26
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-05-12 21:53:51 +0530

    Create leetcode1665.java

---
+++ Problem Practic/Sorting/leetcode1665.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+
+        // Sort by (minimum - actual) descending
+        Arrays.sort(tasks, (a, b) ->
+            (b[1] - b[0]) - (a[1] - a[0])
+        );
+
+        int energy = 0;
+        int current = 0;
+
+        for (int[] task : tasks) {
+
+            int actual = task[0];
+            int minimum = task[1];
+
+            // Need extra energy
+            if (current < minimum) {
+                energy += (minimum - current);
+                current = minimum;
+            }
+
+            // Complete task
+            current -= actual;
+        }
+
+        return energy;
+    }
+}
