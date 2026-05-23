ef3585989225e789642a19bf6b1b34eaca2e3eed
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-05-23 23:05:01 +0530

    Update leetcode1752.java

--- Problem Practic/Questrions Practice/leetcode1752.java
+++ Problem Practic/Questrions Practice/leetcode1752.java
@@ -12,3 +12,23 @@ class Solution {
         return count <= 1;
     }
 }
+
+
+
+// Wrong Solution..........
+
+// class Solution {
+//     public boolean check(int[] nums) {
+//         int n = nums.length;
+//         int temp = 0;
+//         for(int i=0; i<n-1; i++){
+//             if(nums[i] > nums[i+1]){
+//                 temp++;
+//                 if(temp > 1){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
