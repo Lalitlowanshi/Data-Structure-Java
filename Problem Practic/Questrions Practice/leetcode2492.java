10f4c99c5557e3f082ae0c2209328b306799263f
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-07-04 17:40:09 +0530

    Create leetcode2492.java

---
+++ Problem Practic/Questrions Practice/leetcode2492.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minScore(int n, int[][] roads) {
+        List<int[]>[] graph = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int w = road[2];
+
+            graph[u].add(new int[]{v, w});
+            graph[v].add(new int[]{u, w});
+        }
+
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(1);
+        visited[1] = true;
+
+        int ans = Integer.MAX_VALUE;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+
+            for (int[] edge : graph[u]) {
+                int v = edge[0];
+                int w = edge[1];
+
+                ans = Math.min(ans, w);
+
+                if (!visited[v]) {
+                    visited[v] = true;
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
