55ec381ffa6deac34138a05635438851bc2853be
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-07-31 22:43:03 +0530

    Update leetcode3014.java

--- Problem Practic/Questrions Practice/leetcode3014.java
+++ Problem Practic/Questrions Practice/leetcode3014.java
@@ -1,12 +1,44 @@
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int n = word.length();
+//         int ans = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             ans += (i / 8) + 1;
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            ans += (i / 8) + 1;
+        if(n <= 8){
+            ans = n;
         }
-
+        else if( n <= 16){
+            int temp = n%8;
+            if(n%8 == 0){
+                temp = 8;
+            }
+            ans += (temp*2)+8;
+        }
+        else if( n <= 24){
+            int temp = n%8;
+            if(n%8 == 0){
+                temp = 8;
+            }
+            ans += (temp*3)+24;
+        }
+        else{
+            int temp = n%8;
+            ans += (temp*4)+48;
+        }
         return ans;
     }
 }
