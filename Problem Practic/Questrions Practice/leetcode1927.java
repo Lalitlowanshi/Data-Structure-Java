920a1f16b9af6d8e3c57e80e34095d1681118cbf
lalit lowanshi <147048366+Lalitlowanshi@users.noreply.github.com>
2026-08-23 06:04:51 +0530

    Create leetcode1927.java

---
+++ Problem Practic/Questrions Practice/leetcode1927.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = rightQ - leftQ;
+
+        return 2 * diff != 9 * qDiff;
+    }
+}
